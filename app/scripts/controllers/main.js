1dc87519f17de4a31229722a393506c9da77ef24
Christian Brel <ch.brel@gmail.com>
2015-03-06 18:54:07 +0100

    refs ADMINFRONT-5 : Manage encrypted password. Updated BackendSocketFactory.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,19 +8,26 @@
  * Controller of the the6thscreenAdminApp
  */
 angular.module('the6thscreenAdminApp')
-  .controller('MainCtrl', ['$scope', '$http', '$location', 'ADMIN_CONSTANTS', function ($scope, $http, $location, ADMIN_CONSTANTS) {
+  .controller('MainCtrl', ['$scope', '$http', '$location', 'ADMIN_CONSTANTS', 'backendSocket', function ($scope, $http, $location, ADMIN_CONSTANTS, backendSocket) {
 
         $scope.user = {};
 
         $scope.login = function(user) {
             console.log(user);
 
-            $http.post(ADMIN_CONSTANTS.loginBackendUrl, {'usernameOrEmail' : user.usernameOrEmail, 'password' : user.password})
+            var shaObj = new jsSHA(user.password, "TEXT");
+            var encryptedPwd = shaObj.getHash("SHA-512", "HEX");
+
+            console.log(encryptedPwd);
+
+            $http.post(ADMIN_CONSTANTS.loginBackendUrl, {'usernameOrEmail' : user.usernameOrEmail, 'password' : encryptedPwd})
                 .success(function(data, status, headers, config) {
                     console.log("success login");
                     console.log(data);
 
                     $scope.authToken = data.token;
+
+                    backendSocket.init($scope.authToken);
 
                     $location.path('/dashboard');
                 })
--- app/scripts/services/backendSocketFactory.js
+++ app/scripts/services/backendSocketFactory.js
@@ -10,61 +10,116 @@
 angular.module('the6thscreenAdminApp')
     .factory('backendSocket', ['ADMIN_CONSTANTS', 'socketFactory', function (ADMIN_CONSTANTS, socketFactory) {
 
-        //TODO :  Refactor to return an object and add a method to init SocketIO Connection with token...
-
-        /*
         var backendSocketFactory = {}
-        backendSocketFactory.
+        backendSocketFactory.backendSocket = null;
+        backendSocketFactory.token = null;
 
-        var backendIOSocket = io(ADMIN_CONSTANTS.adminBackendUrl,
-            {
-                'reconnection' : true,
-                'reconnectionAttempts' : 10,
-                'reconnectionDelay' : 1000,
-                'reconnectionDelayMax' : 5000,
-                'timeout' : 5000,
-                'autoConnect' : true,
-                'multiplex': false,
-                'query': 'token=' + $scope.authToken
-            });
+        backendSocketFactory.init = function(token) {
+            if(backendSocketFactory.token == null) {
+                backendSocketFactory.token = token;
 
-        backendIOSocket.on("connect", function() {
-            console.info("Connected to Backend.");
-        });
+                var backendIOSocket = io(ADMIN_CONSTANTS.adminBackendUrl,
+                    {
+                        'reconnection': true,
+                        'reconnectionAttempts': 10,
+                        'reconnectionDelay': 1000,
+                        'reconnectionDelayMax': 5000,
+                        'timeout': 5000,
+                        'autoConnect': true,
+                        'multiplex': false,
+                        'query': 'token=' + token
+                    });
 
-        backendIOSocket.on("error", function(errorData) {
-            console.error("An error occurred during connection to Backend.");
-            console.debug(errorData);
-        });
+                backendIOSocket.on("connect", function () {
+                    console.info("Connected to Backend.");
+                });
 
-        backendIOSocket.on("disconnect", function() {
-            console.info("Disconnected to Backend.");
-        });
+                backendIOSocket.on("error", function (errorData) {
+                    console.error("An error occurred during connection to Backend.");
+                    console.debug(errorData);
+                });
 
-        backendIOSocket.on("reconnect", function(attemptNumber) {
-            console.info("Connected to Backend after " + attemptNumber + " attempts.");
-        });
+                backendIOSocket.on("disconnect", function () {
+                    console.info("Disconnected to Backend.");
+                });
 
-        backendIOSocket.on("reconnect_attempt", function() {
-            console.info("Trying to reconnect to Backend.");
-        });
+                backendIOSocket.on("reconnect", function (attemptNumber) {
+                    console.info("Connected to Backend after " + attemptNumber + " attempts.");
+                });
 
-        backendIOSocket.on("reconnecting", function(attemptNumber) {
-            console.info("Trying to connect to Backend - Attempt number " + attemptNumber + ".");
-        });
+                backendIOSocket.on("reconnect_attempt", function () {
+                    console.info("Trying to reconnect to Backend.");
+                });
 
-        backendIOSocket.on("reconnect_error", function(errorData) {
-            console.error("An error occurred during reconnection to Backend.");
-            console.debug(errorData);
-        });
+                backendIOSocket.on("reconnecting", function (attemptNumber) {
+                    console.info("Trying to connect to Backend - Attempt number " + attemptNumber + ".");
+                });
 
-        backendIOSocket.on("reconnect_failed", function() {
-            console.error("Failed to connect to Backend. No new attempt will be done.");
-        });
+                backendIOSocket.on("reconnect_error", function (errorData) {
+                    console.error("An error occurred during reconnection to Backend.");
+                    console.debug(errorData);
+                });
 
-        var backendSocket = socketFactory({
-            ioSocket: backendIOSocket
-        });
+                backendIOSocket.on("reconnect_failed", function () {
+                    console.error("Failed to connect to Backend. No new attempt will be done.");
+                });
 
-        return backendSocket;*/
+                var backendSocket = socketFactory({
+                    ioSocket: backendIOSocket
+                });
+
+                backendSocketFactory.backendSocket = backendSocket;
+            }
+        }
+
+        backendSocketFactory.on = function() {
+            if(backendSocketFactory.backendSocket != null) {
+                backendSocketFactory.backendSocket.on.apply(this,arguments);
+            } else {
+                console.error("An error occurred : BackendSocket isn't initialized.");
+            }
+        };
+
+        backendSocketFactory.addListener = function() {
+            if(backendSocketFactory.backendSocket != null) {
+                backendSocketFactory.backendSocket.addListener.apply(this,arguments);
+            } else {
+                console.error("An error occurred : BackendSocket isn't initialized.");
+            }
+        };
+
+        backendSocketFactory.removeListener = function() {
+            if(backendSocketFactory.backendSocket != null) {
+                backendSocketFactory.backendSocket.removeListener.apply(this,arguments);
+            } else {
+                console.error("An error occurred : BackendSocket isn't initialized.");
+            }
+        };
+
+        backendSocketFactory.removeAllListeners = function() {
+            if(backendSocketFactory.backendSocket != null) {
+                backendSocketFactory.backendSocket.removeAllListeners.apply(this,arguments);
+            } else {
+                console.error("An error occurred : BackendSocket isn't initialized.");
+            }
+        };
+
+        backendSocketFactory.emit = function() {
+            if(backendSocketFactory.backendSocket != null) {
+                backendSocketFactory.backendSocket.emit.apply(this,arguments);
+            } else {
+                console.error("An error occurred : BackendSocket isn't initialized.");
+            }
+        };
+
+        backendSocketFactory.forward = function() {
+            if(backendSocketFactory.backendSocket != null) {
+                backendSocketFactory.backendSocket.forward.apply(this,arguments);
+            } else {
+                console.error("An error occurred : BackendSocket isn't initialized.");
+            }
+        };
+
+
+        return backendSocketFactory;
     }]);
