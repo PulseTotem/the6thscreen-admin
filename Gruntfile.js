d704938b64e25cddcc4ced30945fb0b226701eaa
Christian Brel <ch.brel@gmail.com>
2015-03-10 18:17:33 +0100

    refs ADMINFRONT-14 : Added a task to run serve task without JS Hint check...

--- Gruntfile.js
+++ Gruntfile.js
@@ -394,6 +394,24 @@ module.exports = function (grunt) {
     ]);
   });
 
+    grunt.registerTask('serveWithoutJSHint', 'Compile then start a connect web server without JS Hint check during watch !', function (target) {
+        if (target === 'dist') {
+            return grunt.task.run(['build', 'connect:dist:keepalive']);
+        }
+
+        grunt.task.run([
+            'clean:server',
+            'wiredep',
+            'concurrent:server',
+            'autoprefixer',
+            'connect:livereload',
+            'watch:bower',
+            'watch:styles',
+            'watch:gruntfile',
+            'watch:livereload'
+        ]);
+    });
+
   grunt.registerTask('server', 'DEPRECATED TASK. Use the "serve" task instead', function (target) {
     grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
     grunt.task.run(['serve:' + target]);
