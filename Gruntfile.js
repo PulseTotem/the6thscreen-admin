2a98d37cb1caff3e67945c259c6887b370a6c6da
Christian Brel <ch.brel@gmail.com>
2015-03-10 18:42:58 +0100

    refs ADMINFRONT-14 : Updated serve task to run without JS Hint but it needs a lot of memory...

--- Gruntfile.js
+++ Gruntfile.js
@@ -29,38 +29,48 @@ module.exports = function (grunt) {
 
     // Watches files for changes and runs tasks based on the changed files
     watch: {
-      bower: {
-        files: ['bower.json'],
-        tasks: ['wiredep']
-      },
-      js: {
-        files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
-        tasks: ['newer:jshint:all'],
-        options: {
-          livereload: '<%= connect.options.livereload %>'
-        }
-      },
-      jsTest: {
-        files: ['test/spec/{,*/}*.js'],
-        tasks: ['newer:jshint:test', 'karma']
-      },
-      styles: {
-        files: ['<%= yeoman.app %>/styles/{,*/}*.css'],
-        tasks: ['newer:copy:styles', 'autoprefixer']
-      },
-      gruntfile: {
-        files: ['Gruntfile.js']
-      },
-      livereload: {
-        options: {
-          livereload: '<%= connect.options.livereload %>'
+        bower: {
+            files: ['bower.json'],
+            tasks: ['wiredep']
         },
-        files: [
-          '<%= yeoman.app %>/{,*/}*.html',
-          '.tmp/styles/{,*/}*.css',
-          '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
-        ]
-      }
+        js: {
+            files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
+            tasks: ['newer:jshint:all'],
+            options: {
+              livereload: '<%= connect.options.livereload %>'
+            }
+        },
+        jsTest: {
+            files: ['test/spec/{,*/}*.js'],
+            tasks: ['newer:jshint:test', 'karma']
+        },
+        jsWithoutJSHint: {
+            files: ['<%= yeoman.app %>/scripts/{,*/}*.js'],
+            options: {
+                livereload: '<%= connect.options.livereload %>'
+            }
+        },
+        jsTestWithoutJSHint: {
+            files: ['test/spec/{,*/}*.js'],
+            tasks: ['karma']
+        },
+        styles: {
+            files: ['<%= yeoman.app %>/styles/{,*/}*.css'],
+            tasks: ['newer:copy:styles', 'autoprefixer']
+        },
+        gruntfile: {
+            files: ['Gruntfile.js']
+        },
+        livereload: {
+            options: {
+              livereload: '<%= connect.options.livereload %>'
+            },
+            files: [
+              '<%= yeoman.app %>/{,*/}*.html',
+              '.tmp/styles/{,*/}*.css',
+              '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+            ]
+        }
     },
 
     // The actual grunt server settings
@@ -366,7 +376,20 @@ module.exports = function (grunt) {
         'copy:styles',
         'imagemin',
         'svgmin'
-      ]
+      ],
+      watchWithoutJSHint: {
+          tasks : [
+              'watch:bower',
+              'watch:jsWithoutJSHint',
+              'watch:jsTestWithoutJSHint',
+              'watch:styles',
+              'watch:gruntfile',
+              'watch:livereload'
+          ],
+          options : {
+              limit : 6
+          }
+      }
     },
 
     // Test settings
@@ -405,10 +428,7 @@ module.exports = function (grunt) {
             'concurrent:server',
             'autoprefixer',
             'connect:livereload',
-            'watch:bower',
-            'watch:styles',
-            'watch:gruntfile',
-            'watch:livereload'
+            'concurrent:watchWithoutJSHint'
         ]);
     });
 
