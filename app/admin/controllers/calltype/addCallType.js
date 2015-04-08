8998c05b70f85c23c6238221c4cc834e10a55ad7
Simon Urli <simon.urli@gmail.com>
2015-04-08 15:12:31 +0200

    ADMINFRONT-54 Manage render/receive policy in CallTypes

--- app/admin/controllers/calltype/addCallType.js
+++ app/admin/controllers/calltype/addCallType.js
@@ -52,6 +52,32 @@ angular.module('T6SAdmin')
 
       backendSocket.emit('RetrieveAllRendererDescription');
 
+      backendSocket.emit('RetrieveAllRendererDescription');
+
+      backendSocket.on('AllRenderPolicyDescription', function(response) {
+        callbackManager(response, function (allRenderPolicies) {
+            $scope.renderPolicies = allRenderPolicies;
+          },
+          function (fail) {
+            console.error(fail);
+          }
+        );
+      });
+
+      backendSocket.emit('RetrieveAllRenderPolicyDescription');
+
+      backendSocket.on('AllReceivePolicyDescription', function(response) {
+        callbackManager(response, function (allReceivePolicies) {
+            $scope.receivePolicies = allReceivePolicies;
+          },
+          function (fail) {
+            console.error(fail);
+          }
+        );
+      });
+
+      backendSocket.emit('RetrieveAllReceivePolicyDescription');
+
       backendSocket.on('CallTypeDescription', function(response) {
         callbackManager(response, function (callType) {
             $scope.callType = callType;
