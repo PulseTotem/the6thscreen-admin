538246e2f7749f06f63b542a3a1ad79d04739518
Simon Urli <simon.urli@gmail.com>
2015-05-18 18:53:01 +0200

    ADMINFRONT-60 First interface to graphically create zone

--- app/configuration/controllers/sdi/AddEditSDICtrl.js
+++ app/configuration/controllers/sdi/AddEditSDICtrl.js
@@ -8,7 +8,10 @@
  * Controller of the the6thscreenAdminApp
  */
 angular.module('T6SConfiguration')
-  .controller('T6SConfiguration.AddEditSDICtrl', ['$scope','$rootScope','$routeParams','backendSocket', 'callbackManager', function ($scope, $rootScope, $routeParams, backendSocket, callbackManager) {
+  .controller('T6SConfiguration.AddEditSDICtrl', ['$scope','$rootScope','$routeParams','backendSocket', 'callbackManager', 'saveAttribute', function ($scope, $rootScope, $routeParams, backendSocket, callbackManager, saveAttribute) {
+
+    $scope.zones = [];
+
     backendSocket.on('SDIDescription', function(response) {
       callbackManager(response, function (sdi) {
           $scope.sdi = sdi;
@@ -18,4 +21,37 @@ angular.module('T6SConfiguration')
         }
       );
     });
+
+    backendSocket.on('AnswerCreateZone', function(response) {
+      callbackManager(response, function (zone) {
+          $scope.zones.push(zone);
+          saveAttribute("UpdateSDI", $scope.sdi.id, "addZone", zone.id);
+        },
+        function (fail) {
+          console.error(fail);
+        }
+      );
+    });
+
+    $scope.createZone = function (event) {
+      var div = event.target;
+
+      var divWidth = div.offsetWidth;
+      var divHeight = div.offsetHeight;
+
+      var positionXInDiv = event.offsetX;
+      var positionYInDiv = event.offsetY;
+
+      var relativeX = (positionXInDiv / divWidth) * 100;
+      var relativeY = (positionYInDiv / divHeight) * 100;
+
+      var zone = {
+        width: 10,
+        height: 10,
+        positionFromTop: relativeY,
+        positionFromLeft: relativeX
+      };
+
+      backendSocket.emit("CreateZone", zone);
+    };
   }]);
