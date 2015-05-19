5d4600a542ec7f115b65c81764485db8d152981f
Simon Urli <simon.urli@gmail.com>
2015-05-19 18:27:37 +0200

    ADMINFRONT-63 Request for calltype creation + retrieve sources from service id.

--- app/configuration/controllers/calltype/AddCallTypeCtrl.js
+++ app/configuration/controllers/calltype/AddCallTypeCtrl.js
@@ -8,8 +8,43 @@
  * Controller of the the6thscreenAdminApp
  */
 angular.module('T6SConfiguration')
-  .controller('T6SConfiguration.AddCallTypeCtrl', ['$scope','$routeParams','backendSocket', 'callbackManager', function ($scope, $routeParams, backendSocket, callbackManager, $modalInstance) {
-    $scope.sources = [{"name":"toto", "description": "blabla"}];
+  .controller('T6SConfiguration.AddCallTypeCtrl', ['$scope','$routeParams','backendSocket', 'callbackManager', 'saveAttribute', function ($scope, $routeParams, backendSocket, callbackManager, saveAttribute, $modalInstance) {
+    $scope.sources = [];
+    $scope.callType = null;
+
+    backendSocket.on('SourcesDescriptionFromService', function(response) {
+      callbackManager(response, function (sources) {
+          $scope.sources = sources;
+        },
+        function (fail) {
+          console.error(fail);
+        }
+      );
+    });
+
+    backendSocket.on('AnswerUpdateCallType', function(response) {
+      callbackManager(response, function (callType) {
+          $scope.callType = callType;
+        },
+        function (fail) {
+          console.error(fail);
+        }
+      );
+    });
+
+    backendSocket.on('AnswerCreateCallType', function(response) {
+      callbackManager(response, function (callType) {
+          $scope.callType = callType;
+          saveAttribute("UpdateCallType", $scope.callType.id, "linkZone", $scope.current_zone);
+        },
+        function (fail) {
+          console.error(fail);
+        }
+      );
+    });
+
+    backendSocket.emit("RetrieveSourcesFromServiceId", {"serviceId": $scope.current_service});
+    backendSocket.emit("CreateCallType", {});
+
     console.log("Got the following current zone : "+$scope.zoneId+" or :"+$scope.current_zone);
-    $('#modalCallTypeCreation').modal('show');
   }]);
