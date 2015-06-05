642ca69d20449a19db429ee870561a43ad5055d4
Simon Urli <simon.urli@gmail.com>
2015-06-05 16:17:06 +0200

    ADMINFRONT-94 Refactoring source edition

--- app/admin/controllers/source/AddEditSourceCtrl.js
+++ app/admin/controllers/source/AddEditSourceCtrl.js
@@ -8,7 +8,49 @@
  * Controller of the the6thscreenAdminApp
  */
 angular.module('T6SAdmin')
-  .controller('T6SAdmin.AddEditSourceCtrl', ['$scope','$routeParams','backendSocket', 'callbackManager', 'saveAttribute', function ($scope, $routeParams, backendSocket, callbackManager, saveAttribute) {
+  .controller('T6SAdmin.AddEditSourceCtrl', ['$scope','$routeParams','backendSocket', 'callbackManager', 'saveAttribute', '$filter', function ($scope, $routeParams, backendSocket, callbackManager, saveAttribute, $filter) {
+
+    $scope.allInfoTypes = [];
+    $scope.allServices = [];
+    $scope.availableParameters = [];
+    $scope.selectedParam = null;
+
+    backendSocket.on('AllInfoTypeDescription', function(response) {
+      callbackManager(response, function (allInfoTypes) {
+          $scope.allInfoTypes = allInfoTypes;
+        },
+        function (fail) {
+          console.error(fail);
+        }
+      );
+    });
+
+    backendSocket.emit('RetrieveAllInfoTypeDescription');
+
+    backendSocket.on('AllServiceDescription', function(response) {
+      callbackManager(response, function (allServices) {
+          $scope.allServices = allServices;
+        },
+        function (fail) {
+          console.error(fail);
+        }
+      );
+    });
+
+    backendSocket.emit('RetrieveAllServiceDescription');
+
+    backendSocket.on('AllParamTypeDescription', function(response) {
+      callbackManager(response, function (allParamTypes) {
+          $scope.availableParameters = allParamTypes;
+          $scope.updateParamType();
+        },
+        function (fail) {
+          console.error(fail);
+        }
+      );
+    });
+
+    backendSocket.emit('RetrieveAllParamTypeDescription');
 
     backendSocket.on('AnswerUpdateSource', function(response) {
       callbackManager(response, function (source) {
@@ -22,6 +64,42 @@ angular.module('T6SAdmin')
 
     $scope.saveSourceAttribute = function (element, value) {
       saveAttribute("UpdateSource", $scope.source.id, element, value);
+    };
+
+    $scope.updateParamType = function () {
+      $scope.availableParameters = $filter('filter')($scope.availableParameters, function (value) {
+        var filteredList = $filter('filter')($scope.source.paramTypes, {id: value.id});
+        return filteredList.length == 0;
+      });
+    };
+
+    $scope.linkParamType = function () {
+      if ($scope.selectedParam != null) {
+        $scope.source.paramTypes.push($scope.selectedParam);
+        $scope.saveSourceAttribute("linkParamType", $scope.selectedParam.id);
+        $scope.selectedParam = null;
+      }
+    };
+
+    $scope.removeParamType = function (paramId) {
+      $scope.saveSourceAttribute("unlinkParamType", paramId);
+      $scope.source.paramTypes = $filter('filter')($scope.source.paramTypes, {id: !paramId});
+    };
+
+    $scope.showSelectedInfoType = function () {
+      if (!$scope.source.infoType) {
+        return 'Not set';
+      }
+      var selected = $filter('filter')($scope.allInfoTypes, {id: $scope.source.infoType.id});
+      return ($scope.source.infoType && selected.length) ? selected[0].name : 'Not set';
+    };
+
+    $scope.showSelectedService = function () {
+      if (!$scope.source.service) {
+        return 'Not set';
+      }
+      var selected = $filter('filter')($scope.allServices, {id: $scope.source.service.id});
+      return ($scope.source.service && selected.length) ? selected[0].name : 'Not set';
     };
 
     $scope.close = function () {
