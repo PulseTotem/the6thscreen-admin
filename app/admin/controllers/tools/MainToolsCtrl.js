b8e79433dcf00cc918a989eefea75e2c4f62f0d9
Simon Urli <simon.urli@gmail.com>
2016-07-26 18:54:24 +0200

    ADMINFRONT-186 Add a button in tools to check orphans

--- app/admin/controllers/tools/MainToolsCtrl.js
+++ app/admin/controllers/tools/MainToolsCtrl.js
@@ -21,8 +21,29 @@ angular.module('T6SAdmin')
       );
     });
 
+    backendSocket.on('AnswerCheckAndRemoveOrphans', function(response) {
+      callbackManager(response, function (result) {
+          $scope.result = result;
+          $scope.resultCheckData = true;
+          $scope.analysing = false;
+        },
+        function (fail) {
+          console.error(fail);
+        }
+      );
+    });
+
     $scope.checkAllData = function () {
       backendSocket.emit('CheckAllData', {});
       $scope.analysing = true;
+      $scope.result = null;
+      $scope.resultCheckData = false;
+    };
+
+    $scope.checkAndRemoveOrphans = function () {
+      backendSocket.emit('CheckAndRemoveOrphans', {});
+      $scope.analysing = true;
+      $scope.result = null;
+      $scope.resultCheckData = false;
     };
   }]);
